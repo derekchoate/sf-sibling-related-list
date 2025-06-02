9b03674014a1e6cd26d78367165cbe661ff677a3
Derek Choate <derek.choate@gmail.com>
2025-06-02 15:49:34 +0100

    changed metadata for the community and removed unnecessary apex classes

--- force-app/main/default/lwc/siblingRelatedList/siblingRelatedList.js
+++ force-app/main/default/lwc/siblingRelatedList/siblingRelatedList.js
@@ -5,52 +5,113 @@ import { NavigationMixin } from "lightning/navigation";
 
 export default class SiblingRelatedList extends NavigationMixin(LightningElement) {
 
+    /**
+     * Id of the record being viewed
+     * @type {string}
+     */
     @api
     recordId;
 
+    /**
+     * SObject Type of the record being viewed
+     * @type {string}
+     */
     @api
     sObjectTypeName;
 
+    /**
+     * SObject Type of the parent
+     * @type {string}
+     */
     @api
     parentSObjectTypeName;
 
-    @api
-    parentRecordId;
-
+    /**
+     * API Name of the field that holds the parent id
+     * @type {string}
+     */
     @api
     parentIdField;
 
+    /**
+     * API name of the child relationship on the parent object that relates to the child records to display
+     * @type {string} 
+     */
     @api
     relationshipName;
 
+    /**
+     * Id of the parent record
+     * @type {string}
+     */
+    parentRecordId;
+
+    /**
+     * Summary info about the related list, includes things like the icon and colour
+     * @type {Object<string, *>} Related List Info Summary https://developer.salesforce.com/docs/atlas.en-us.uiapi.meta/uiapi/ui_api_responses_related_list_summary.htm
+     */
     relatedListSummary;
 
+    /**
+     * Detail information about the related list, included the column definitions
+     * @type {Object<string, *>} Related List Info https://developer.salesforce.com/docs/atlas.en-us.uiapi.meta/uiapi/ui_api_responses_related_list_metadata.htm
+     */
     relatedListInfo;
 
+    /**
+     * Columns to display
+     * @type {[Object<string, *>]} Array of columns
+     */
     displayColumns;
 
+    /**
+     * an array of field names (derived from the displayColumns)
+     * @type {[string]} Array of filed names
+     */
     @track
     relatedListFieldNames;
 
+    /**
+     * The current page of records (from getRelatedRecords)
+     * @type {Object<string, *>} Related List Record Collection https://developer.salesforce.com/docs/atlas.en-us.uiapi.meta/uiapi/ui_api_responses_related_list_record_collection.htm
+     */
     page;
 
+    /**
+     * The array of records to display
+     * @type {[Object<string, *>]} Array of Records https://developer.salesforce.com/docs/atlas.en-us.uiapi.meta/uiapi/ui_api_responses_record.htm#ui_api_responses_record
+     */
     records;
 
-    connected = false;
-
+    /**
+     * Field definition for the Parent ID Field
+     * @type {Object<string, *>} fieldDefintion
+     */
     get parentIdFieldDefinition() {
         return {"fieldApiName":this.parentIdField,
                 "objectApiName":this.sObjectTypeName};
     }
 
+    /**
+     * Parent ID Field in an array
+     * @type {[Object<string, *>]} fieldDefintions
+     */
     get parentIdFieldArray() {
         return [this.parentIdFieldDefinition];
     }
 
+    /**
+     * URL of the related list icon
+     * @type {string} url
+     */
     get iconUrl() {
         return this.relatedListSummary?.themeInfo?.iconUrl ?? '';
     }
 
+    /**
+     * HEX Color code for the icon
+     * @type {string}
+     */
     get iconColor() {
         if (this.relatedListSummary?.themeInfo?.color) {
             return '#' + this.relatedListSummary.themeInfo.color;
@@ -58,14 +119,25 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         return '';
     }
 
+    /**
+     * Plural label of the related list object
+     * @type {string}
+     */
     get entityLabelPlural() {
         return this.relatedListSummary?.entityLabelPlural ?? '';
     }
 
+    /**
+     * Related list label
+     * @type {string}
+     */
     get relatedListLabel() {
         return this.relatedListSummary?.label ?? '';
     }
 
+    /**
+     * Gets the parentId for this record
+     */
     @wire (getRecord, {
         recordId: "$recordId",
         fields: "$parentIdFieldArray"
@@ -80,6 +152,9 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         }
     }
 
+    /**
+     * Gets the related list summary for the child relationship
+     */
     @wire(getRelatedListsInfo, {
         parentObjectApiName: "$parentSObjectTypeName"
     })
@@ -94,6 +169,9 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         }
     }
 
+    /**
+     * Gets the related list info for the child relationship
+     */
     @wire(getRelatedListInfo, {
         parentObjectApiName: "$parentSObjectTypeName",
         relatedListId: "$relationshipName"
@@ -114,6 +192,9 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         }
     }
 
+    /**
+     * Gets the related list records for the child relationship
+     */
     @wire(getRelatedListRecords, {
         parentRecordId: "$parentRecordId",
         relatedListId: "$relationshipName",
@@ -122,7 +203,7 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
     async handleGetRelatedListRecords({ error, data }) {
         if (data) {
             this.page = data;
-            this.records = await this.composeDisplayRecords(data.records);
+            this.records = await this.prepareDisplayRecords(data.records);
         }
         else if (error) {
             console.error('An error occurred whilst retrieving the related list records');
@@ -130,27 +211,38 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         }
     }
 
+    /**
+     * Sets the icon background colour
+     */
     setIconColor() {
         if (this.refs.relatedListIcon) {
             this.refs.relatedListIcon.style.backgroundColor = this.iconColor;
         }
     }
 
-    async composeDisplayRecords(relatedListRecords) {
+    /**
+     * Modifies the records such that the data structure is compatible with the lightning datatable
+     */
+    async prepareDisplayRecords(relatedListRecords) {
         let records = [];
 
+        //If the related list records isn't an array, then return
         if (!Array.isArray(relatedListRecords) ||
             !Array.isArray(this.displayColumns)) {
             return records;
         }
 
+        //Loop through the records and map each one
         for (const recordData of relatedListRecords) {
             let record = {
                 id: recordData.id
             };
 
-            
+            //Loop throug the fields to get the values
             for (const field of this.displayColumns) {
+
+                //If the data type is string, then get the display value, 
+                // otherwise allow the components to format the value correctly
                 if (field.type === 'string') {
                     record[field.fieldApiName] = getFieldDisplayValue(recordData, field.apiPath);
                 }
@@ -158,18 +250,24 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
                     record[field.fieldApiName] = getFieldValue(recordData, field.apiPath);
                 }
                 
+                //If the field is a lookup, set the url
                 if (field.lookupId) {
                     record[`${field.fieldApiName}-resourceUrl`] = await this.generateRecordUrl(recordData, field);
                 }
             }
             records.push(record);
         }
-        
-        console.log('records...');
+
         console.log(JSON.stringify(records, null, 5));
+        
         return records;
     }
 
+    /**
+     * Map the columns to the expected input by the lightning datatable
+     * @param {Object<string, *>} relatedListInfo 
+     * @returns {[Object<string, *>]} list of fields to display in the related list
+     */
     prepareColumns(relatedListInfo) {
         let fields = [];
 
@@ -179,11 +277,20 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         }
 
         relatedListInfo.displayColumns.forEach(col => {
+
+            //clone the original column
             let field = structuredClone(col);
+
+            //set the API path which is used later on to get the field value
             field.apiPath = `${relatedListInfo.objectApiNames[0]}.${field.fieldApiName}`;
+
+            //map the fieldApiName to the fieldName
             field.fieldName = field.fieldApiName;
+
+            //map the fieldType
             field.type = this.getColumnType(field);
 
+            //If the field is a lookup field, set additional properties to render the url properly
             if (field.lookupId) {
                 field.fieldName = `${field.fieldName}-resourceUrl`;
                 field.typeAttributes = {
@@ -202,28 +309,43 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
         return fields;
     }
 
+    /**
+     * Generate a link to a record (used to create links in lookup fields)
+     * @param {Object<string, *>} recordData from the getRelatedRecords api
+     * @param {*} field from the display columns
+     * @returns {string} url to the record
+     */
     async generateRecordUrl(recordData, field) {
 
         try {
+
+            //if the field isn't a lookup field, return nothing
             if (!field.lookupId) {
                 return undefined;
             }
 
+            //get the field that hols the lookup relationship
             let lookupId = field.lookupId;
 
             let recordId;
 
+            //if the field relates to the related record, then use it's record ID
             if (lookupId === 'Id') {
                 recordId = recordData.id;
             }
 
+            //otherwise navigate to the field that contains the related id
             else {
+
+                //remove ".Id" from the end of the field path
                 if (lookupId.endsWith('.Id')) {
                     lookupId = lookupId.substring(0, lookupId.length - 3);
                 }
 
+                //start with the fields node on the record data
                 let currentVal = recordData.fields;
 
+                //loop through each element of the path and get the corresponding value node
                 for (const objKey of lookupId.split('\\.')) {
                     currentVal = currentVal[objKey]?.value;
                     if (!currentVal) {
@@ -232,18 +354,22 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
                     }
                 }
 
+                //if a value node wasn't found, return nothing
                 if (!currentVal) {
                     return undefined;
                 }
 
-                recordId = currentVal?.id;
+                //otherwise set the record id to the id of the current value
+                recordId = currentVal.id;
             }
             
+            //just in case no record id was found, return nothing
             if (!recordId) {
                 console.error(`could not find record id`);
                 return undefined;
             }
             
+            //generate the url and return it
             return await this[NavigationMixin.GenerateUrl]({
                 type: "standard__recordPage",
                 attributes: {
@@ -253,12 +379,17 @@ export default class SiblingRelatedList extends NavigationMixin(LightningElement
             });
         }
         catch (ex) {
-            console.log('an error occurred whilst generating a record url');
-            console.log(JSON.stringify(ex, null, 5));
+            console.error('an error occurred whilst generating a record url');
+            console.error(JSON.stringify(ex, null, 5));
             return undefined;
         }
     }
 
+    /**
+     * Get the field type for the lightning datatable
+     * @param {string} column
+     * @returns {string} data type
+     */
     getColumnType(column) {
 
         if (column.lookupId) {
